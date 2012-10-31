5a9ba7f483014e0f3cef8d092cb9662a0ec313fe
Michael Pritchard <blueghostuk@gmail.com>
2012-10-31 23:05:08 +0000

    tidy classes

--- NetworkRailDownloader.WebApi/Controllers/StanoxController.cs
+++ NetworkRailDownloader.WebApi/Controllers/StanoxController.cs
@@ -1,11 +1,8 @@
 ﻿using NetworkRailDownloader.ServiceLayer;
 using NetworkRailDownloader.WebApi.ActionFilters;
 using NetworkRailDownloader.WebApi.ViewModels;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace NetworkRailDownloader.WebApi.Controllers
