6b7c40d88a75b3fd5d9cbff833dcb50e4ba5cbcd
Michael Pritchard <blueghostuk@gmail.com>
2012-11-01 20:46:59 +0000

    add new repo for stanox

---
+++ NetworkRailDownloader/NetworkRailDownloader.ServiceLayer/StanoxRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkRailDownloader.ServiceLayer
+{
+    public class StanoxRepository : DbRepository
+    {
+        public string GetStanoxByCrs(string crs)
+        {
+            const string sql = "SELECT Stanox FROM Tiploc WHERE CRS = @crs";
+
+            return ExecuteScalar<string>(sql, new { crs });
+        }
+    }
+}
