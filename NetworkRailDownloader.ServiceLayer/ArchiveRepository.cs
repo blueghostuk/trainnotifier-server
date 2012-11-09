c9bf0beb168a4e67ab86f792a4955d4a12751ad2
Michael Pritchard <blueghostuk@gmail.com>
2012-11-09 20:35:58 +0000

    search by wttid #27

---
+++ NetworkRailDownloader.ServiceLayer/ArchiveRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainNotifier.ServiceLayer;
+
+namespace TrainNotifier.Service
+{
+    public class ArchiveRepository : DbRepository
+    {
+        public IEnumerable<dynamic> GetBtWttId(string wttId)
+        {
+            const string sql = @"
+                SELECT
+                    `TrainId`,
+                    `origin_dep_timestamp`,
+                    `sched_origin_stanox`,
+                    `sched_wtt_id`
+                FROM `LiveTrain`
+                WHERE `sched_wtt_id` LIKE @wttId";
+
+            wttId += "%";
+
+            return Query<dynamic>(sql, new { wttId });
+        }
+    }
+}
---
+++ NetworkRailDownloader.WebApi/Controllers/ArchiveController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TrainNotifier.Console.WebApi.ViewModels;
+using TrainNotifier.Service;
+
+namespace TrainNotifier.Console.WebApi.Controllers
+{
+    public class ArchiveController : ApiController
+    {
+        public IEnumerable<WttIdSearchResult> GetByWttId(string wttId)
+        {
+            return new ArchiveRepository().GetBtWttId(wttId)
+                .Select(r => new WttIdSearchResult
+                {
+                    WttId = r.sched_wtt_id,
+                    TrainId = r.TrainId,
+                    From = r.sched_origin_stanox,
+                    Depart = r.origin_dep_timestamp
+                });
+        }
+    }
+}
