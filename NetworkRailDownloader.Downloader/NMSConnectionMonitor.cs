2aeeeaac6ee0098e50caea55e648a5f2eaa46059
Michael Pritchard <blueghostuk@gmail.com>
2012-11-10 11:27:21 +0000

    remove timeout checking, connection will handlt itself

--- NetworkRailDownloader.Downloader/NMSConnectionMonitor.cs
+++ NetworkRailDownloader.Downloader/NMSConnectionMonitor.cs
@@ -7,8 +7,6 @@ namespace TrainNotifier.Common.NMS
 {
     public class NMSConnectionMonitor : IDisposable
     {
-        private static readonly TimeSpan _defaultTs = TimeSpan.FromMinutes(1);
-
         private DateTime? _lastMsgRecd;
         private Timer _timer;
 
@@ -25,14 +23,14 @@ namespace TrainNotifier.Common.NMS
         }
 
         public static NMSConnectionMonitor MonitorConnection(IConnection connection, IMessageConsumer consumer,
-            AutoResetEvent resetEvent, TimeSpan? timeout = null)
+            AutoResetEvent resetEvent)
         {
             NMSConnectionMonitor monitor = new NMSConnectionMonitor();
-            monitor.MonitorConnection(connection, consumer, resetEvent, timeout ?? _defaultTs);
+            monitor.MonitorConnectionLocal(connection, consumer, resetEvent);
             return monitor;
         }
 
-        private void MonitorConnection(IConnection connection, IMessageConsumer consumer, AutoResetEvent resetEvent, TimeSpan timeout)
+        private void MonitorConnectionLocal(IConnection connection, IMessageConsumer consumer, AutoResetEvent resetEvent)
         {
             consumer.Listener += (m) =>
             {
@@ -45,18 +43,6 @@ namespace TrainNotifier.Common.NMS
                 QuitOk = false;
                 resetEvent.Set();
             };
-
-            _timer = new Timer((state) =>
-            {
-                if (!connection.IsStarted ||
-                    !_lastMsgRecd.HasValue ||
-                    _lastMsgRecd.Value >= (DateTime.UtcNow.Add(timeout)))
-                {
-                    Trace.TraceInformation("Connection Monitor: Connection Stopped or No data recd for {0}, closing connection", timeout);
-                    QuitOk = false;
-                    resetEvent.Set();
-                }
-            }, null, timeout, timeout);
         }
 
         public void Dispose()
