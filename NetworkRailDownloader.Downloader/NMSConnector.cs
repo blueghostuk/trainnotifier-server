1af991d81c70bed16f7701640a9d2c65c97ef01b
Michael Pritchard <blueghostuk@gmail.com>
2012-12-30 13:43:10 +0000

    add basics for schedule service #43

--- NetworkRailDownloader.Downloader/NMSConnector.cs
+++ NetworkRailDownloader.Downloader/NMSConnector.cs
@@ -29,34 +29,6 @@ namespace TrainNotifier.Common.NMS
                 .CreateConnection(ConfigurationManager.AppSettings["Username"], ConfigurationManager.AppSettings["Password"]);
         }
 
-        public void DownloadSchedule(string filePath, Toc toc = Toc.All, ScheduleType schedule = ScheduleType.Full, DayOfWeek? day = null)
-        {
-            Uri requestUri = new Uri(string.Format("{0}?type={1}&day={2}", (object)ConfigurationManager.AppSettings["ScheduleUri"], (object)string.Format("CIF_{0}_{1}_DAILY", (object)TocHelper.TocToString(toc), (object)TocHelper.ScheduleTypeToString(schedule)), (object)TocHelper.ScheduleTypeToDay(schedule, day)));
-            byte[] buffer = new byte[4096];
-            WebRequest webRequest = WebRequest.Create(requestUri);
-            webRequest.Timeout = (int)TimeSpan.FromMinutes((double)int.Parse(ConfigurationManager.AppSettings["ScheduleTimeoutMins"])).TotalMilliseconds;
-            string str = Convert.ToBase64String(Encoding.ASCII.GetBytes(ConfigurationManager.AppSettings["Username"] + ":" + ConfigurationManager.AppSettings["Password"]));
-            webRequest.Headers[HttpRequestHeader.Authorization] = "Basic " + str;
-            using (WebResponse response = webRequest.GetResponse())
-            {
-                using (Stream responseStream = response.GetResponseStream())
-                {
-                    using (MemoryStream memoryStream = new MemoryStream())
-                    {
-                        int count;
-                        do
-                        {
-                            count = responseStream.Read(buffer, 0, buffer.Length);
-                            memoryStream.Write(buffer, 0, count);
-                        }
-                        while (count != 0);
-                        byte[] bytes = memoryStream.ToArray();
-                        System.IO.File.WriteAllBytes(filePath, bytes);
-                    }
-                }
-            }
-        }
-
         public void SubscribeToFeed(Feed feed)
         {
             try
---
+++ TrainNotifier.Schedule.Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainNotifier.ScheduleLibrary;
+
+namespace TrainNotifier.Schedule.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScheduleService.DownloadSchedule("");
+        }
+    }
+}
