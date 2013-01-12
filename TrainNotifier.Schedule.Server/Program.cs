8aa23d7b9cf820a4e8544fc0c9ed4ddd26062996
Michael Pritchard <blueghostuk@gmail.com>
2013-01-12 19:20:32 +0000

    initial work to download schedules

--- TrainNotifier.Schedule.Server/Program.cs
+++ TrainNotifier.Schedule.Server/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,45 @@ namespace TrainNotifier.Schedule.Server
     {
         static void Main(string[] args)
         {
-            ScheduleService.DownloadSchedule("");
+            string tempDir = Path.GetTempPath();
+            string gzFile = Path.Combine(tempDir, string.Format("{0:ddmmyyyy}.gz", DateTime.UtcNow));
+            string jsonFile = Path.Combine(tempDir, string.Format("{0:ddmmyyyy}.json", DateTime.UtcNow));
+            try
+            {
+                ScheduleService.DownloadSchedule(gzFile);
+                using (FileStream originalFileStream = File.OpenRead(gzFile))
+                {
+                    using (FileStream decompressedFileStream = File.Create(jsonFile))
+                    {
+                        using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                        {
+                            Console.WriteLine("Decompressing {0} to {1}", gzFile, jsonFile);
+                            decompressionStream.CopyTo(decompressedFileStream);
+                            Console.WriteLine("Decompressed: {0}", jsonFile);
+                        }
+                    }
+                }
+                if (File.Exists(jsonFile))
+                {
+                    foreach (string row in File.ReadLines(jsonFile))
+                    {
+                        var rowData = JsonConvert.DeserializeObject<dynamic>(row);
+                        try
+                        {
+                            if (rowData.JsonScheduleV1 != null)
+                            {
+
+                            }
+                        }
+                        catch { }
+                    }
+                }
+            }
+            finally
+            {
+                File.Delete(gzFile);
+                File.Delete(jsonFile);
+            }
         }
     }
 }
