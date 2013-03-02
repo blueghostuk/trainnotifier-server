3048989ff60ae57c73a3bf345c3632679c6f9a75
Michael Pritchard <blueghostuk@gmail.com>
2013-03-02 00:23:20 +0000

    cache schedule data #52

--- NetworkRailDownloader.WebApi/Controllers/ScheduleController.cs
+++ NetworkRailDownloader.WebApi/Controllers/ScheduleController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Web.Http;
 using TrainNotifier.Common.Model.Schedule;
+using TrainNotifier.Console.WebApi.ActionFilters;
 using TrainNotifier.Service;
 
 namespace TrainNotifier.Console.WebApi.Controllers
 {
     public class ScheduleController : ApiController
     {
+        // cache for 12 hrs
+        [CachingActionFilterAttribute(60 * 60 * 12)]
         public ScheduleTrain GetByUid(string trainId, string trainUid)
         {
             ScheduleRepository sr = new ScheduleRepository();
