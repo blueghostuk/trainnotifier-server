9110a5616825b65f107cbc54b5a076be72cc853f
Michael Pritchard <blueghostuk@gmail.com>
2013-03-26 14:21:03 +0000

    add new classes for web api

---
+++ NetworkRailDownloader.Common/Model/Reinstatement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+using TrainNotifier.Common.Model.Schedule;
+
+namespace TrainNotifier.Common.Model
+{
+    [DataContract]
+    public class Reinstatement
+    {
+        [DataMember]
+        public TiplocCode NewOrigin { get; set; }
+
+        [DataMember]
+        public DateTime? NewDepartureTime { get; set; }
+    }
+}
