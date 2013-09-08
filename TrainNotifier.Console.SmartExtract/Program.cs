d83cf2b42d74571d3f1cf4ce7f0d982c9d0fb329
Michael Pritchard <blueghostuk@gmail.com>
2013-09-08 17:51:51 +0100

    Add TD Smart Extract Parser

---
+++ TrainNotifier.Console.SmartExtract/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+
+namespace TrainNotifier.Console.SmartExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this data is fetchable via http://nrodwiki.rockshore.net/index.php/ReferenceData
+            string jsonData = File.ReadAllText("SMARTExtract.json");
+
+            TDContainer container = JsonConvert.DeserializeObject<TDContainer>(jsonData);
+
+            System.Console.WriteLine("Loaded {0} TD Elements", container.BERTHDATA.Count());
+
+            System.Console.WriteLine("Enter Query");
+            string data;
+            while (!string.IsNullOrEmpty((data = System.Console.ReadLine())))
+            {
+                //var clauses = data.Split(',');
+                //if (!clauses.Any())
+                //    break;
+
+                var results = container.BERTHDATA
+                    .Where(t => t.TD == data)
+                    .OrderBy(t => t.STANOX)
+                    .ThenBy(t => t.PLATFORM)
+                    .ThenBy(t => t.EventType);
+                System.Console.WriteLine(string.Join("", Enumerable.Repeat("=", 68).ToArray()));
+
+                System.Console.WriteLine("TD".PadRight(4) +
+                "F-B".PadRight(6) +
+                "T-B".PadRight(6) +
+                "F-L".PadRight(4) +
+                "T-L".PadRight(4) +
+                "STANOX".PadRight(8) +
+                "P".PadRight(4) +
+                "EVENT-TYPE".PadRight(12) +
+                "STEP-TYPE".ToString().PadRight(18));
+                System.Console.WriteLine(string.Join("", Enumerable.Repeat("=", 68).ToArray()));
+
+                foreach (var result in results)
+                {
+                    System.Console.WriteLine(result);
+                }
+                System.Console.WriteLine(string.Join("", Enumerable.Repeat("=", 68).ToArray()));
+
+            }
+        }
+    }
+}
