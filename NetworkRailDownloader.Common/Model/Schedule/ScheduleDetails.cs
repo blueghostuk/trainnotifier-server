fe5541fa19307bf11c523417f461f4798a5adb9f
Michael Pritchard <blueghostuk@gmail.com>
2013-10-18 21:39:30 +0100

    add ScheduleDetails class

---
+++ NetworkRailDownloader.Common/Model/Schedule/ScheduleDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TrainNotifier.Common.Model.Schedule
+{
+    public class ScheduleDetails
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool Deleted { get; set; }
+        public bool RunsMonday { get; set; }
+        public bool RunsTuesday { get; set; }
+        public bool RunsWednesday { get; set; }
+        public bool RunsThursday { get; set; }
+        public bool RunsFriday { get; set; }
+        public bool RunsSaturday { get; set; }
+        public bool RunsSunday { get; set; }
+    }
+}
