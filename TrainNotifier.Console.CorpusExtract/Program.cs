073e6b6dc69ae2971eca92ae2590661d4d0df5a9
Michael Pritchard <blueghostuk@gmail.com>
2013-10-18 21:34:32 +0100

    add corpus extract processor

---
+++ TrainNotifier.Console.CorpusExtract/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using TrainNotifier.Service;
+
+namespace TrainNotifier.Console.CorpusExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this data is fetchable via http://nrodwiki.rockshore.net/index.php/ReferenceData
+            string jsonData = File.ReadAllText("CORPUSExtract.json");
+
+            TiplocContainer container = JsonConvert.DeserializeObject<TiplocContainer>(jsonData);
+
+            System.Console.WriteLine("Loaded {0} Tiplocs", container.TIPLOCDATA.Count());
+
+            TiplocRepository tiplocRepo = new TiplocRepository();
+
+            ulong counter = 1;
+            foreach (var tiploc in container.TIPLOCDATA)
+            {
+                System.Console.WriteLine("Processing number {0}", counter);
+                if (!string.IsNullOrWhiteSpace(tiploc.STANOX) && 
+                    !string.IsNullOrWhiteSpace(tiploc.TIPLOC) && 
+                    tiplocRepo.GetTiplocByStanox(tiploc.STANOX) == null)
+                {
+                    tiplocRepo.InsertTiploc(tiploc.ToTiplocCode());
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ TrainNotifier.Console.CorpusExtract/Tiploc.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using TrainNotifier.Common.Model.Schedule;
+
+namespace TrainNotifier.Console.CorpusExtract
+{
+    public class TiplocContainer
+    {
+        public IEnumerable<Tiploc> TIPLOCDATA { get; set; }
+    }
+
+    public class Tiploc
+    {
+        public string STANOX { get; set; }
+        public string UIC { get; set; }
+        [JsonProperty(PropertyName="3ALPHA")]
+        public string ALPHA { get; set; }
+        public string TIPLOC { get; set; }
+        public string NLC { get; set; }
+        public string NLCDESC { get; set; }
+        public string NLCDESC16 { get; set; }
+
+        public TiplocCode ToTiplocCode()
+        {
+            return new TiplocCode
+            {
+                Tiploc = TIPLOC.Trim(),
+                Stanox = STANOX.Trim(),
+                Nalco = string.IsNullOrWhiteSpace(NLC) ? null : NLC.Trim(),
+                Description = string.IsNullOrWhiteSpace(NLCDESC) ? null : NLCDESC.Trim(),
+                CRS = string.IsNullOrWhiteSpace(ALPHA) ? null : ALPHA.Trim()
+            };
+        }
+    }
+}
