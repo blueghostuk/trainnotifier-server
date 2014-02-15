22a07733dd7a8ea37fa5b0967c97d7165b059b19
Michael Pritchard <blueghostuk@gmail.com>
2014-02-15 00:56:49 +0000

    misc updates

--- TrainNotifier.Console.SmartExtract/Program.cs
+++ TrainNotifier.Console.SmartExtract/Program.cs
@@ -4,7 +4,11 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using TrainNotifier.Common.Model;
+using TrainNotifier.Common.Model.CorpusExtract;
+using TrainNotifier.Common.Model.Schedule;
 using TrainNotifier.Common.Model.SmartExtract;
+using TrainNotifier.Service;
 
 namespace TrainNotifier.Console.SmartExtract
 {
@@ -26,6 +30,9 @@ namespace TrainNotifier.Console.SmartExtract
             {
                 BERTHDATA = allSmartData
             };
+            ILookup<string, TDElement> tdElementsByArea = allSmartData
+                .Where(td => !string.IsNullOrEmpty(td.STANOX))
+                .ToLookup(td => td.TD);
 
             if (args.Length > 0 && args[0].Equals("extract", System.StringComparison.InvariantCultureIgnoreCase))
             {
@@ -35,12 +42,26 @@ namespace TrainNotifier.Console.SmartExtract
                 return;
             }
 
+            TiplocRepository _tiplocRepo = new TiplocRepository();
+
+            var dbTiplocs = _tiplocRepo.Get();
+
+            string tiplocData = File.ReadAllText("CORPUSExtract.json");
+            ILookup<string, TiplocCode> tiplocByStanox = JsonConvert.DeserializeObject<TiplocContainer>(tiplocData).TIPLOCDATA
+                .Select(t => t.ToTiplocCode())
+                .ToLookup(t => t.Stanox);
+
             System.Console.WriteLine("Loaded {0} TD Elements", container.BERTHDATA.Count());
 
             System.Console.WriteLine("Enter Query");
             string data;
             while (!string.IsNullOrEmpty((data = System.Console.ReadLine())))
             {
+                if (data.StartsWith("query=", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    GenerateQuery(data, tdElementsByArea, tiplocByStanox);
+                    continue;
+                }
                 var clauses = data.Split(',');
                 if (!clauses.Any())
                     break;
@@ -68,7 +89,7 @@ namespace TrainNotifier.Console.SmartExtract
                     "STANME".PadRight(10) +
                     "P".PadRight(4) +
                     "EVENT-TYPE".PadRight(12) +
-                    "STEP-TYPE".ToString().PadRight(10) + 
+                    "STEP-TYPE".ToString().PadRight(10) +
                     "OFFSET".ToString().PadRight(6));
                 System.Console.WriteLine(string.Join("", Enumerable.Repeat("=", 72).ToArray()));
 
@@ -79,6 +100,45 @@ namespace TrainNotifier.Console.SmartExtract
                 System.Console.WriteLine(string.Join("", Enumerable.Repeat("=", 72).ToArray()));
 
             }
+        }
+
+        private static void GenerateQuery(string data, ILookup<string, TDElement> tdElementsByArea, ILookup<string, TiplocCode> tiplocByStanox)
+        {
+            data = data.Replace("query=", string.Empty);
+            // TODO: parse query
+
+            UpdateTrainMovements(new TrainDescriber[] 
+            {
+                new CcTD
+                {
+                    AreaId = "WY",
+                    Description = "1H14",
+                    Time = DateTime.UtcNow,
+                    To = "B014",
+                    Type = "From"
+                }
+            }, tdElementsByArea, tiplocByStanox);
+        }
+
+        private static void UpdateTrainMovements(IEnumerable<TrainDescriber> trainData, ILookup<string, TDElement> tdElementsByArea, ILookup<string, TiplocCode> tiplocByStanox)
+        {
+            var data = trainData
+                .Where(tdesc => tdElementsByArea.Contains(tdesc.AreaId))
+                .Select(tdesc => Tuple.Create(tdesc, tdElementsByArea[tdesc.AreaId].FirstOrDefault(td => td.Equals(tdesc))))
+                .Where(t => t.Item2 != null)
+                .Select(t => Tuple.Create(t.Item1, t.Item2, tiplocByStanox[t.Item2.STANOX].FirstOrDefault()))
+                .Where(t => t.Item3 != null)
+                .ToList();
+
+            foreach (var td in data)
+            {
+                ProcessTdResult(td);
+            }
+        }
+
+        private static void ProcessTdResult(Tuple<TrainDescriber, TDElement, TiplocCode> td)
+        {
+            //throw new NotImplementedException();
         }
     }
 }
