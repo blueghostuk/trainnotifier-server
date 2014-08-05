4d274e27e65bb99361e03a7c36589bd7e12368fe
Michael Pritchard <blueghostuk@gmail.com>
2014-08-05 23:26:49 +0100

    add delay model class

---
+++ TrainNotifier.Common.Model/Api/Delay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.Serialization;
+using TrainNotifier.Common.Model.Schedule;
+
+namespace TrainNotifier.Common.Model.Api
+{
+    [DataContract]
+    public class Delay
+    {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string Headcode { get; set; }
+        [DataMember]
+        public string Uid { get; set; }
+        [DataMember]
+        public int DelayTime { get; set; }
+        [DataMember]
+        public string OriginStanox { get; set; }
+        [DataMember]
+        public string DestStanox { get; set; }
+        [DataMember]
+        public AtocCode Operator { get; set; }
+        [DataMember]
+        public StationStop From { get; set; }
+        [DataMember]
+        public StationStop To { get; set; }
+    }
+
+    [DataContract]
+    public class StationStop
+    {
+        [DataMember]
+        public string Platform { get; set; }
+        [DataMember]
+        public DateTime Expected { get; set; }
+        [DataMember]
+        public DateTime Actual { get; set; }
+    }
+}
