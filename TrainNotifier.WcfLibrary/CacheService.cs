295d7e73a858ab8233064727f754a0f741d290c4
Michael Pritchard <blueghostuk@gmail.com>
2015-11-02 20:09:09 +0000

    dont run async anymore

--- TrainNotifier.WcfLibrary/CacheService.cs
+++ TrainNotifier.WcfLibrary/CacheService.cs
@@ -34,115 +34,106 @@ namespace TrainNotifier.WcfLibrary
 
         public void CacheTrainData(IEnumerable<ITrainData> trainData)
         {
-            Task.Run(() =>
+            try
             {
-                try
-                {
-                    _cacheDb.BatchInsertTrainData(trainData);
-                }
-                catch (Exception e)
-                {
-                    Trace.TraceError("Could not save Train Data: {0}", e);
-                }
-            });
+                _cacheDb.BatchInsertTrainData(trainData);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Could not save Train Data: {0}", e);
+            }
         }
 
         public void CacheVSTPSchedule(ScheduleTrain train)
         {
-            Task.Run(() =>
+            try
             {
-                try
-                {
-                    _scheduleRepository.InsertSchedule(train, ScheduleSource.VSTP);
-                }
-                catch (Exception e)
-                {
-                    Trace.TraceError("Could not save VSTP Train: {0}", e);
-                }
-            });
+                _scheduleRepository.InsertSchedule(train, ScheduleSource.VSTP);
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Could not save VSTP Train: {0}", e);
+            }
         }
 
         public void CachePPMData(RtppmData data)
         {
-            Task.Run(() =>
-            {
-                if (data == null)
-                    return;
-                Trace.TraceInformation("Saving PPM Data for {0}", data.Timestamp);
+            if (data == null)
+                return;
+            Trace.TraceInformation("Saving PPM Data for {0}", data.Timestamp);
 
-                if (data.NationalPPM != null)
+            if (data.NationalPPM != null)
+            {
+                var nationalPPMId = _sectors
+                    .Where(s => s.OperatorCode == null)
+                    .Where(s => s.SectorCode == null)
+                    .Select(s => s.PPMSectorId)
+                    .SingleOrDefault();
+                if (nationalPPMId != null && nationalPPMId != Guid.Empty)
                 {
-                    var nationalPPMId = _sectors
+                    SavePPMData(data.NationalPPM, nationalPPMId, data.Timestamp);
+                }
+                else
+                {
+                    Trace.TraceError("PPM: Could not find National PPM in Database");
+                }
+            }
+            if (data.Sectors != null)
+            {
+                foreach (var nationalSector in data.Sectors)
+                {
+                    var id = _sectors
                         .Where(s => s.OperatorCode == null)
-                        .Where(s => s.SectorCode == null)
+                        .Where(s => s.SectorCode == nationalSector.Code)
                         .Select(s => s.PPMSectorId)
                         .SingleOrDefault();
-                    if (nationalPPMId != null && nationalPPMId != Guid.Empty)
+                    if (id != null && id != Guid.Empty)
                     {
-                        SavePPMData(data.NationalPPM, nationalPPMId, data.Timestamp);
+                        SavePPMData(nationalSector, id, data.Timestamp);
                     }
                     else
                     {
-                        Trace.TraceError("PPM: Could not find National PPM in Database");
+                        Trace.TraceError("PPM: Could not find Sector {0} in Database", nationalSector.Code);
                     }
                 }
-                if (data.Sectors != null)
+            }
+            if (data.Operators != null)
+            {
+                foreach (var toc in data.Operators)
                 {
-                    foreach (var nationalSector in data.Sectors)
+                    var id = _sectors
+                        .Where(s => Convert.ToByte(s.OperatorCode) == Convert.ToByte(toc.Code))
+                        .Where(s => s.SectorCode == null)
+                        .Select(s => s.PPMSectorId)
+                        .SingleOrDefault();
+                    if (id != null && id != Guid.Empty)
                     {
-                        var id = _sectors
-                            .Where(s => s.OperatorCode == null)
-                            .Where(s => s.SectorCode == nationalSector.Code)
-                            .Select(s => s.PPMSectorId)
-                            .SingleOrDefault();
-                        if (id != null && id != Guid.Empty)
-                        {
-                            SavePPMData(nationalSector, id, data.Timestamp);
-                        }
-                        else
-                        {
-                            Trace.TraceError("PPM: Could not find Sector {0} in Database", nationalSector.Code);
-                        }
+                        SavePPMData(toc, id, data.Timestamp);
                     }
-                }
-                if (data.Operators != null)
-                {
-                    foreach (var toc in data.Operators)
+                    else
                     {
-                        var id = _sectors
+                        Trace.TraceError("PPM: Could not find TOC {0} in Database", toc.Code);
+                    }
+                    foreach (var tocSector in toc.ServiceGroups)
+                    {
+                        var sectorId = _sectors
                             .Where(s => Convert.ToByte(s.OperatorCode) == Convert.ToByte(toc.Code))
-                            .Where(s => s.SectorCode == null)
+                            .Where(s => s.SectorCode != null)
+                            .Where(s => s.SectorCode.Equals(tocSector.Code, StringComparison.InvariantCultureIgnoreCase))
+                            .Where(s => s.Description.Equals(tocSector.Name, StringComparison.InvariantCultureIgnoreCase))
                             .Select(s => s.PPMSectorId)
                             .SingleOrDefault();
-                        if (id != null && id != Guid.Empty)
+                        if (sectorId != null && sectorId != Guid.Empty)
                         {
-                            SavePPMData(toc, id, data.Timestamp);
+                            SavePPMData(tocSector, sectorId, data.Timestamp);
                         }
                         else
                         {
-                            Trace.TraceError("PPM: Could not find TOC {0} in Database", toc.Code);
-                        }
-                        foreach (var tocSector in toc.ServiceGroups)
-                        {
-                            var sectorId = _sectors
-                                .Where(s => Convert.ToByte(s.OperatorCode) == Convert.ToByte(toc.Code))
-                                .Where(s => s.SectorCode != null)
-                                .Where(s => s.SectorCode.Equals(tocSector.Code, StringComparison.InvariantCultureIgnoreCase))
-                                .Where(s => s.Description.Equals(tocSector.Name, StringComparison.InvariantCultureIgnoreCase))
-                                .Select(s => s.PPMSectorId)
-                                .SingleOrDefault();
-                            if (sectorId != null && sectorId != Guid.Empty)
-                            {
-                                SavePPMData(tocSector, sectorId, data.Timestamp);
-                            }
-                            else
-                            {
-                                Trace.TraceError("PPM: Could not find TOC {0} Sector {1}-{2} in Database", toc.Code, tocSector.Name, tocSector.Code);
-                            }
+                            Trace.TraceError("PPM: Could not find TOC {0} Sector {1}-{2} in Database", toc.Code, tocSector.Name, tocSector.Code);
                         }
                     }
                 }
-            });
+            }
         }
 
         private static void SavePPMData(PPMRecord record, Guid id, DateTime ts)
